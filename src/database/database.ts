5ecd8a847d9203f4b9fa1f0d5705ee4534fc5f58
DavidSheltonSF <david.faria012@gmail.com>
2026-02-13 23:41:14 -0300

    feat(database): add PostgreHelper

---
+++ src/database/database.ts
@@ -0,0 +1,46 @@
+import { Client, ClientConfig, QueryResult } from "pg";
+
+export class PostgreHelper {
+  static instance: PostgreHelper | null = null;
+  private client: Client | null = null;
+
+  private constructor(config: ClientConfig){
+     this.client = new Client(config)
+  }
+
+  static getInstance(): PostgreHelper {
+    if(this.instance) return this.instance;
+    
+    this.instance =  new PostgreHelper({
+      host: "localhost",
+      user: "postgres",
+      port: 5432,
+      password: "admin",
+      database: "AutoflexChallenge"
+    });
+
+    return this.instance;
+  }
+
+  async connect(){
+    if(!this.client) {
+      throw Error("Could not connect because PostgreHelper.client is null");
+    };
+    await this.client.connect();
+  }
+
+  async query(queryString: string): Promise<QueryResult<any>>{
+    if(!this.client) {
+      throw Error("Could not make query because PostgreHelper.client is null");
+    };
+    return await this.client.query(queryString);
+  }
+
+  async disconnect(){
+    if(!this.client) {
+      throw Error("Could not disconnect because PostgreHelper.client is null");
+    };
+    await this.client.end();
+    this.client = null;
+  }
+}
