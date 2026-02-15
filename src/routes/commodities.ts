6fc5502846cef54eeb58f25b1e7eebfcbb6ff19c
DavidSheltonSF <david.faria012@gmail.com>
2026-02-15 18:57:19 -0300

    feat(routes): add commodities-related routes

---
+++ src/routes/commodities.ts
@@ -0,0 +1,102 @@
+import { Router, type Request, type Response } from 'express';
+import { dbConnection } from '..';
+
+export function configCommoditiesRoutes(router: Router) {
+  router.post('/commodities', async (req: Request, res: Response) => {
+    if (!req.body) {
+      return res.status(400).json({
+        message: 'Missing request body',
+      });
+    }
+
+    const { code, name, quantity } = req.body;
+    if (!code || !name || !quantity) {
+      return res.status(400).json({
+        message: 'Missing required fields',
+      });
+    }
+
+    const query = {
+      text: `INSERT INTO commodities(code, name, quantity) VALUES($1, $2, $3) RETURNING *`,
+      values: [code, name, quantity],
+    };
+    const result = await dbConnection.query(query);
+    console.log(result);
+
+    return res.status(201).json({
+      data: result.rows[0],
+    });
+  });
+
+  router.get('/commodities', async (req: Request, res: Response) => {
+    const result = await dbConnection.query(`SELECT * FROM commodities`);
+    return res.status(200).json({
+      data: result.rows,
+    });
+  });
+
+  router.get('/commodities/:id', async (req: Request, res: Response) => {
+    const commodityId = req.params.id;
+    if (!commodityId) {
+      return res.status(400).json({
+        message: 'Missing commodities id',
+      });
+    }
+
+    const result = await dbConnection.query(`SELECT * FROM commodities WHERE id = ${commodityId}`);
+    return res.status(200).json({
+      data: result.rows,
+    });
+  });
+
+  router.put('/commodities/:id', async (req: Request, res: Response) => {
+    const commodityId = req.params.id;
+
+    if (!commodityId) {
+      return res.status(400).json({
+        message: 'Missing commodities id',
+      });
+    }
+
+    if (!req.body) {
+      return res.status(400).json({
+        message: 'Missing request body',
+      });
+    }
+
+    const { code, name, quantity } = req.body;
+    if (!code || !name || !quantity) {
+      return res.status(400).json({
+        message: 'Missing required fields',
+      });
+    }
+
+    const query = {
+      text: `UPDATE commodities SET code =  $1, name = $2, quantity = $3 WHERE id = ${commodityId} RETURNING *`,
+      values: [code, name, quantity],
+    };
+
+    const result = await dbConnection.query(query);
+
+    return res.status(200).json({
+      message: 'UPDATED',
+      data: result.rows
+    });
+  });
+
+  router.delete('/commodities/:id', async (req: Request, res: Response) => {
+    const commodityId = req.params.id;
+    if(!commodityId){
+      res.status(400).json({
+        message: "Missing commodity id"
+      })
+    }
+
+
+    const result = await dbConnection.query(`DELETE FROM commodities WHERE id = ${commodityId} RETURNING *`);
+    return res.status(200).json({
+      message: "DELETED",
+      data: result.rows,
+    });
+  });
+}
--- src/routes/configRouter.ts
+++ src/routes/configRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { configProductsRoutes } from './products';
+import { configCommoditiesRoutes } from './commodities';
 
 export function configRouter(): Router {
   const router = Router();
   configProductsRoutes(router);
+  configCommoditiesRoutes(router);
   return router;
 }
