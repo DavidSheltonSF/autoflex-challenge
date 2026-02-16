e439dfa9cc618ceab7167528bcbb6f5095ba4e8d
DavidSheltonSF <david.faria012@gmail.com>
2026-02-16 19:39:48 -0300

    feat(data): add fake products

---
+++ frontend/fakeData/products.ts
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 1,
+    code: 'asee5555',
+    name: 'Wood Table',
+    price: 130.5,
+    quantity: 200,
+  },
+  {
+    id: 2,
+    code: 'as585855',
+    name: 'Plastic chair',
+    price: 80,
+    quantity: 800,
+  },
+  {
+    id: 3,
+    code: 'asee7775',
+    name: 'Fan',
+    price: 180.5,
+    quantity: 20,
+  },
+  {
+    id: 4,
+    code: '77785555',
+    name: 'Wooden door',
+    price: 5300.5,
+    quantity: 55,
+  },
+];
