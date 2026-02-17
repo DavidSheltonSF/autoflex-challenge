2d1f48381c66f205dc2b48fbb85dc097a6ad4765
DavidSheltonSF <david.faria012@gmail.com>
2026-02-17 20:12:31 -0300

    feat(services): add fetchDeletePRoductById

---
+++ frontend/services/fetchDeleteProductById.ts
@@ -0,0 +1,14 @@
+export async function fetchDeleteProductById(id: number) {
+  const response = await fetch(`http://localhost:3002/products/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    console.log(errorText);
+    throw Error(errorText);
+  }
+
+  const json = await response.json();
+  return json.data;
+}
