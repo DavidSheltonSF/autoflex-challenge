320b9af11a7f8702c0192ef3f66972b22e5dd871
DavidSheltonSF <david.faria012@gmail.com>
2026-02-18 15:23:31 -0300

    refactor(BaseModal): show an loading animated icon while loading

--- frontend/components/modals/BaseModal.tsx
+++ frontend/components/modals/BaseModal.tsx
@@ -1,13 +1,15 @@
 import { ReactNode, useRef } from 'react';
 import { CloseIcon } from '../icons/CloseIcon';
+import { LoadingIcon } from '../icons/LoadingIcon';
 
 interface Props {
   close: Function;
   children: ReactNode;
   additionalStyles: string;
+  isLoading?: boolean;
 }
 
-export function BaseModal({ close, children, additionalStyles }: Props) {
+export function BaseModal({ close, children, additionalStyles, isLoading }: Props) {
   const modalRef = useRef<HTMLDivElement>(null);
 
   function handleCloseModal() {
@@ -20,7 +22,7 @@ export function BaseModal({ close, children, additionalStyles }: Props) {
   return (
     <div
       ref={modalRef}
-      className={`flex flex-col fixed top-[10%] left-[50%] translate-x-[-50%] bg-color-white shadow-[0_0_3px_black] rounded-lg fade-in-animation ${additionalStyles}`}
+      className={`flex flex-col fixed top-[10%] left-[50%] translate-x-[-50%] bg-color-white shadow-[0_0_3px_black] rounded-lg fade-in-animation ${additionalStyles} min-h-[20vh]`}
     >
       <div className="flex justify-end items-center p-[8px] border-b">
         <button
@@ -30,7 +32,14 @@ export function BaseModal({ close, children, additionalStyles }: Props) {
           <CloseIcon width="32px" height="32px" />
         </button>
       </div>
-      <div className="flex size-full p-[16px]">{children}</div>
+      {isLoading ? (
+        <span className='m-auto animate-spin'>
+          {' '}
+          <LoadingIcon height="56px" width='56px' />
+        </span>
+      ) : (
+        <div className="flex size-full p-[16px] fade-in-animation">{children}</div>
+      )}
     </div>
   );
 }
