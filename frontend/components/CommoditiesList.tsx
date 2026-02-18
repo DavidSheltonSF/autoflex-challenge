c4166f705ba52a39d5159717bbaba39c58c737f2
DavidSheltonSF <david.faria012@gmail.com>
2026-02-18 19:39:48 -0300

    refactor(CommoditiesList):
    - Make add button open AddCommodityModal
    - Fetch commodities from the API
    - Make it render skeletons while fething commodities

--- frontend/components/CommoditiesList.tsx
+++ frontend/components/CommoditiesList.tsx
@@ -1,14 +1,56 @@
-import { CommodityCard } from './CommodityCard';
+'use client';
 import { Commodity } from '@/types/Commodity';
+import { CommodityCard } from './CommodityCard';
+import { AddButton } from './AddButton';
+import { useContext } from 'react';
+import { CardSkeleton } from './CardSkeleton';
+import { AddCommodityModalContext } from '@/contexts/commodities/AddCommodityModalContext';
+import { WithId } from '@/types/WithId';
 
 interface Props {
-  commodities: Commodity[];
+  commodities: WithId<Commodity>[];
+  isLoading: boolean;
 }
 
-export function CommoditiesList({ commodities }: Props) {
-  const renderProducts = commodities.map((commodity, index) => {
+export function CommoditiesList({ commodities, isLoading }: Props) {
+  const renderCommodities = commodities.map((commodity, index) => {
     return <CommodityCard key={index} {...commodity} />;
   });
 
-  return <div className="flex flex-col gap-[24px] w-full">{renderProducts}</div>;
+  const renderSkeleton = () => {
+    const skeletons = [];
+
+    for (let i = 0; i < 4; i++) {
+      skeletons.push(<CardSkeleton key={i} />);
+    }
+
+    return skeletons;
+  };
+
+  const context = useContext(AddCommodityModalContext);
+  if (!context) {
+    throw Error('Missing AddCommodityModalContext');
+  }
+
+  const { setModalState } = context;
+
+  return (
+    <div className="flex flex-col gap-[24px] w-full">
+      <AddButton
+        label="Add new Commodity"
+        width="200px"
+        onClick={() => {
+          setModalState({ isOpen: true });
+        }}
+      />
+      <div className="flex flex-col gap-[24px]">
+        {commodities.length === 0 && (
+          <div className="text-2xl text-center">
+            <h1>No Commodities where found</h1>
+          </div>
+        )}
+        {isLoading ? renderSkeleton() : renderCommodities}
+      </div>
+    </div>
+  );
 }
