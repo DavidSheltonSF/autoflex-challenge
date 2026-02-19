ad77a0eec7fc679610d52c03c9b2b0134b2e0eca
DavidSheltonSF <david.faria012@gmail.com>
2026-02-19 16:01:36 -0300

    feat(components): add ProductCommoditiesSerchBar

---
+++ frontend/components/ProductCommoditiesSearchBar.tsx
@@ -0,0 +1,66 @@
+import { fetchCommodities } from '@/services/fetchCommodities';
+import { Commodity } from '@/types/Commodity';
+import { WithId } from '@/types/WithId';
+import { Dispatch, SetStateAction, useContext, useEffect, useRef, useState } from 'react';
+
+interface Props {
+  setCommodityId: Dispatch<SetStateAction<string | null>>;
+}
+
+export function ProductCommoditiesSearchBar({ setCommodityId }: Props) {
+  const [searchText, setSearchText] = useState('');
+  const [searchContainerIsOpen, setSearchContainerIsOpen] = useState(false);
+  const [commodities, setCommodities] = useState<WithId<Commodity>[]>([]);
+  const searchBarRef = useRef<HTMLInputElement>(null);
+  const searchContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    async function loadCommodities() {
+      try {
+        const result = await fetchCommodities();
+        const filteredResult = result.filter((commodity) => commodity.name.startsWith(searchText));
+        setCommodities(filteredResult);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    loadCommodities();
+  }, [searchText]);
+
+  const renderCommodities = commodities.map((commodity, index) => {
+    return (
+      <ul
+        key={index}
+        className="cursor-pointer bg-color-white hover:brightness-90"
+        onClick={() => {
+          setCommodityId(commodity.id);
+          setSearchText(commodity.name);
+          searchBarRef.current!.value = commodity.name;
+          setSearchContainerIsOpen(false);
+        }}
+      >
+        {commodity.name}
+      </ul>
+    );
+  });
+  return (
+    <div className="relative h-[30px] w-[60%]">
+      <input
+        ref={searchBarRef}
+        onChange={(e) => setSearchText(e.target.value)}
+        onFocus={() => setSearchContainerIsOpen(true)}
+        className="w-full border rounded-md"
+        type="text"
+      />
+      <div
+        ref={searchContainerRef}
+        className={`absolute bg-color-white top-full right-0 w-full rounded-md overflow-hidden ${
+          searchContainerIsOpen ? 'h-auto' : 'h-0'
+        }`}
+      >
+        <ul className="flex flex-col flex flex-col p-[8px] gap-[4px]">{renderCommodities}</ul>
+      </div>
+    </div>
+  );
+}
