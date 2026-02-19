581efdcce93ad9075f20a677dbb5969cf2fbf2b6
DavidSheltonSF <david.faria012@gmail.com>
2026-02-19 16:34:12 -0300

    refactor(Input): add ref prop and specify onChange and onFocus with HTMLInputElement

--- frontend/components/Input.tsx
+++ frontend/components/Input.tsx
@@ -1,6 +1,7 @@
-import { ChangeEventHandler, FocusEventHandler } from 'react';
+import { ChangeEventHandler, FocusEventHandler, Ref } from 'react';
 
 interface Props {
+  ref?: Ref<HTMLInputElement>;
   height?: string;
   width?: string;
   name?: string;
@@ -8,15 +9,26 @@ interface Props {
   placeholder?: string;
   defaultValue?: string;
   maxLength?: number;
-  onChange?: ChangeEventHandler;
-  onFocus?: FocusEventHandler;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  onFocus?: FocusEventHandler<HTMLInputElement>;
 }
 
 export function Input(props: Props) {
-  const { height, width, name, type, placeholder, defaultValue, maxLength, onChange, onFocus } =
-    props;
+  const {
+    ref,
+    height,
+    width,
+    name,
+    type,
+    placeholder,
+    defaultValue,
+    maxLength,
+    onChange,
+    onFocus,
+  } = props;
   return (
     <input
+      ref={ref}
       name={name}
       type={type}
       className="w-full border rounded-md py-[4px] px-[8px]"
