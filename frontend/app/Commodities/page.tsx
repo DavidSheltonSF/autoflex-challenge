0afb72b6374e2d889fb9bc1980da9b309cc17a6b
DavidSheltonSF <david.faria012@gmail.com>
2026-02-20 01:06:31 -0300

    feat(pages): add Commodities page

---
+++ frontend/app/Commodities/page.tsx
@@ -0,0 +1,12 @@
+import { CommoditiesSection } from '@/components/dynamicSections/CommoditiesSection';
+
+export default function Commodities() {
+  return (
+    <div className="flex flex-col min-h-screen items-start justify-start bg-zinc-50 font-sans">
+      <div className="size-full flex-col px-[80px] py-[40px]">
+        <h1 className="text-3xl font-bold my-[16px] text-black">Commodities</h1>
+        <CommoditiesSection />
+      </div>
+    </div>
+  );
+}
