83366e30132f3cf4dd670a766a0436b5b9b87fe1
DavidSheltonSF <david.faria012@gmail.com>
2026-02-20 01:27:46 -0300

    fix(Navbar): import the correct item component

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,15 +1,15 @@
 import Link from 'next/link';
-import { NewNavItem } from './NewNavItem';
+import { NavItem } from './NavItem';
 
 export function Navbar() {
   return (
     <nav className="flex w-full justify-center p-[8px] bg-gray-800 text-white">
       <ul className="flex gap-[12px] text-xl">
         <Link href="/">
-          <NewNavItem name="products" />
+          <NavItem name="products" />
         </Link>
         <Link href="/Commodities">
-          <NewNavItem name="commodities" />
+          <NavItem name="commodities" />
         </Link>
       </ul>
     </nav>
