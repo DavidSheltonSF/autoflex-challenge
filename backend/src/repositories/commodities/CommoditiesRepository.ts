a3773bf61bd20255e8f156513ac138e0ddaec8fc
DavidSheltonSF <david.faria012@gmail.com>
2026-02-20 05:55:15 -0300

    fix(CommoditiesRepository): fix methods types and names

--- backend/src/repositories/commodities/CommoditiesRepository.ts
+++ backend/src/repositories/commodities/CommoditiesRepository.ts
@@ -2,9 +2,10 @@ import { Commodity } from '../../types/Commodity';
 import { WithId } from '../../types/WithId';
 
 export interface CommoditiesRepository {
-  findAll: () => Promise<WithId<Commodity[]>>;
-  findById: () => Promise<WithId<Commodity>>;
+  findAll: () => Promise<WithId<Commodity>[]>;
+  findById: (id: string) => Promise<WithId<Commodity>>;
   create: (commodity: Commodity) => Promise<WithId<Commodity>>;
-  update: (id: string, commodity: Commodity) => Promise<WithId<Commodity>>;
-  delete: (id: string) => Promise<WithId<Commodity>>;
+  updateById: (id: string, commodity: Commodity) => Promise<WithId<Commodity>>;
+  deleteById: (id: string) => Promise<WithId<Commodity>>;
+
 }
