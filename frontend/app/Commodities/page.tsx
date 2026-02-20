5044f03079557b26114448f4860d29723a8ed504
DavidSheltonSF <david.faria012@gmail.com>
2026-02-20 01:27:18 -0300

    refactor(pages): enhance responsiveness

--- frontend/app/Commodities/page.tsx
+++ frontend/app/Commodities/page.tsx
@@ -3,7 +3,7 @@ import { CommoditiesSection } from '@/components/dynamicSections/CommoditiesSect
 export default function Commodities() {
   return (
     <div className="flex flex-col min-h-screen items-start justify-start bg-zinc-50 font-sans">
-      <div className="size-full flex-col px-[80px] py-[40px]">
+      <div className="size-full justify-center items-center flex-co px-[16px] min-lg:px-[80px] py-[40px]">
         <h1 className="text-3xl font-bold my-[16px] text-black">Commodities</h1>
         <CommoditiesSection />
       </div>
