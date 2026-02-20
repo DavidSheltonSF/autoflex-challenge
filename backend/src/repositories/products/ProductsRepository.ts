27074fc80f92715c769a0efc239aaf5f819dea2c
DavidSheltonSF <david.faria012@gmail.com>
2026-02-20 05:53:27 -0300

    fix(ProductsRepository):  rename update to updataById and delete to deleteById

--- backend/src/repositories/products/ProductsRepository.ts
+++ backend/src/repositories/products/ProductsRepository.ts
@@ -5,6 +5,6 @@ export interface ProductsRepository {
   findAll: () => Promise<WithId<Product>[]>;
   findById: (id: string) => Promise<WithId<Product>>;
   create: (product: Product) => Promise<WithId<Product>>;
-  update: (id: string, product: Product) => Promise<WithId<Product>>;
-  delete: (id: string) => Promise<WithId<Product>>;
+  updateById: (id: string, product: Product) => Promise<WithId<Product>>;
+  deleteById: (id: string) => Promise<WithId<Product>>;
 }
