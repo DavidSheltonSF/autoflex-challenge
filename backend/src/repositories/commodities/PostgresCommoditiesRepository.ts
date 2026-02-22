cd4a73048aea0127503f940ee46d3fe603cdf894
DavidSheltonSF <david.faria012@gmail.com>
2026-02-21 22:03:19 -0300

    refactor(PostgressCommoditiesRepository): check if the id provided is numeric in findById, updateById and deleteById

--- backend/src/repositories/commodities/PostgresCommoditiesRepository.ts
+++ backend/src/repositories/commodities/PostgresCommoditiesRepository.ts
@@ -1,6 +1,7 @@
 import { PostgreHelper } from '../../database/database';
 import { Commodity } from '../../types/Commodity';
 import { WithId } from '../../types/WithId';
+import { checkIdIsNumeric } from '../helpers/checkIdIsNumeric';
 import { CommoditiesRepository } from './CommoditiesRepository';
 
 export const dbConnection = PostgreHelper.getInstance();
@@ -10,21 +11,32 @@ export class PostgresCommoditiesRepository implements CommoditiesRepository {
     const result = await dbConnection.query('SELECT * FROM commodities');
     const rows = result.rows;
     const mappedCommodities: WithId<Commodity>[] = rows.map((commodity) => {
-      return { id: commodity.id, code: commodity.code, name: commodity.name, quantity: commodity.quantity };
+      return {
+        id: commodity.id,
+        code: commodity.code,
+        name: commodity.name,
+        quantity: commodity.quantity,
+      };
     });
     return mappedCommodities;
   }
 
   async findById(id: string): Promise<WithId<Commodity> | null> {
+    checkIdIsNumeric(id);
     const result = await dbConnection.query(`SELECT * FROM commodities WHERE id = ${id}`);
     const rows = result.rows;
 
     if (rows.length === 0) {
-      return null
+      return null;
     }
 
     const commodity = rows[0];
-    return { id: commodity.id, code: commodity.code, name: commodity.name, quantity: commodity.quantity };
+    return {
+      id: commodity.id,
+      code: commodity.code,
+      name: commodity.name,
+      quantity: commodity.quantity,
+    };
   }
 
   async create(commodity: Commodity): Promise<WithId<Commodity>> {
@@ -44,6 +56,7 @@ export class PostgresCommoditiesRepository implements CommoditiesRepository {
   }
 
   async updateById(id: string, commodity: Commodity): Promise<WithId<Commodity>> {
+    checkIdIsNumeric(id);
     const { code, name, quantity } = commodity;
     const query = {
       text: `UPDATE commodities SET code =  $1, name = $2, quantity = $3 WHERE id = ${id} RETURNING *`,
@@ -61,9 +74,15 @@ export class PostgresCommoditiesRepository implements CommoditiesRepository {
   }
 
   async deleteById(id: string): Promise<WithId<Commodity>> {
+    checkIdIsNumeric(id);
     const result = await dbConnection.query(`DELETE FROM commodities WHERE id = ${id} RETURNING *`);
     const rows = result.rows;
     const commodity = rows[0];
-    return { id: commodity.id, code: commodity.code, name: commodity.name, quantity: commodity.quantity };
+    return {
+      id: commodity.id,
+      code: commodity.code,
+      name: commodity.name,
+      quantity: commodity.quantity,
+    };
   }
 }
