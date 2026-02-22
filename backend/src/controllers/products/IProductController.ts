faaf86221a4eec090d1154fd759d4c964d0660b2
DavidSheltonSF <david.faria012@gmail.com>
2026-02-22 16:02:41 -0300

    refactor(IProductController): make updateById return Promise<updateByIdResponse> and deleteById return <deleteByIdResponse>

--- backend/src/controllers/products/IProductController.ts
+++ backend/src/controllers/products/IProductController.ts
@@ -1,13 +1,16 @@
-import { HttpResponse } from '../types/HttpResponse';
-import { Product } from '../../types/Product';
-import { WithId } from '../../types/WithId';
 import { HttpRequest } from '../types/HttpRequest';
-import { createResponse, findAllResponse, findByIdResponse } from './responses';
+import {
+  createResponse,
+  deleteByIdResponse,
+  findAllResponse,
+  findByIdResponse,
+  updateByIdResponse,
+} from './responses';
 
 export interface IProductController {
   findAll: (httpRequest: HttpRequest) => Promise<findAllResponse>;
   findById: (httpRequest: HttpRequest) => Promise<findByIdResponse>;
   create: (httpRequest: HttpRequest) => Promise<createResponse>;
-  updateById: (httpRequest: HttpRequest) => Promise<HttpResponse<WithId<Product>>>;
-  deleteById: (httpRequest: HttpRequest) => Promise<HttpResponse<WithId<Product>>>;
+  updateById: (httpRequest: HttpRequest) => Promise<updateByIdResponse>;
+  deleteById: (httpRequest: HttpRequest) => Promise<deleteByIdResponse>;
 }
