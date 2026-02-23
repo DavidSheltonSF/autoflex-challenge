88ae22623a9a86d80b51ca0c6294f250b110406c
DavidSheltonSF <david.faria012@gmail.com>
2026-02-23 00:37:43 -0300

    refactor(frontend): type AvailableProductsList useState  with ProductsAndCommodity

--- frontend/components/AvailableProductsList.tsx
+++ frontend/components/AvailableProductsList.tsx
@@ -1,12 +1,11 @@
 'use client';
 import { fetchAvailableProducts } from '@/services/fetchAvailableProducts';
-import { Product } from '@/types/Product';
-import { WithId } from '@/types/WithId';
 import { useEffect, useState } from 'react';
 import { LabelValue } from './LabelValue';
+import { ProductAndCommodity } from '@/types/ProductAndCommodity';
 
 export function AvailableProductsList() {
-  const [products, setProducts] = useState<WithId<Product & { quantity: number }>[]>();
+  const [products, setProducts] = useState<ProductAndCommodity[]>();
   useEffect(() => {
     async function loadProducts() {
       try {
@@ -26,10 +25,10 @@ export function AvailableProductsList() {
         key={index}
         className="flex flex-col gap-[16px] w-full wbg-white border border-black/80 p-[16px] rounded-lg"
       >
-        <h1 className="text-xl">{product.name}</h1>
+        <h1 className="text-xl">{product.productName}</h1>
         <div className="flex gap-[8px] text-lg">
           <LabelValue label="price" value={product.price} />
-          <LabelValue label="quantity" value={product.quantity} />
+          <LabelValue label="quantity" value={product.commodityQuantity} />
           <div className="flex flex-col gap-[8px] min-lg:flex-row justify-between"></div>
         </div>
       </article>
