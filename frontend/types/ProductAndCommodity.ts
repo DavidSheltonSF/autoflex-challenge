d7fd1d289c98fd5b479598d57b2db02d6613e514
DavidSheltonSF <david.faria012@gmail.com>
2026-02-23 00:36:35 -0300

    feat(frontend/types): add ProductAndCommodity

---
+++ frontend/types/ProductAndCommodity.ts
@@ -0,0 +1,8 @@
+export interface ProductAndCommodity {
+  productId: string;
+  productName: string;
+  productCode: string;
+  price: number;
+  commodityQuantity: number;
+  availableCommodityQuantity: number;
+}
