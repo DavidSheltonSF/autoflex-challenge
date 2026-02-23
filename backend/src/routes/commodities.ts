7b936957472b70caa1a9ccee7366649a7487efb6
DavidSheltonSF <david.faria012@gmail.com>
2026-02-22 21:47:24 -0300

    refactor(routes/commodities): use CommoditiesController to handle routes

--- backend/src/routes/commodities.ts
+++ backend/src/routes/commodities.ts
@@ -1,117 +1,13 @@
-import { Router, type Request, type Response } from 'express';
-import { dbConnection } from '..';
-import { PostgresCommoditiesRepository } from '../repositories/commodities/PostgresCommoditiesRepository';
+import { Router } from 'express';
+import { makeCommoditiesController } from '../controllers/factories/makeCommoditiesController';
+import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 
-const commoditiesRepository = new PostgresCommoditiesRepository();
+const commoditiesController = makeCommoditiesController();
 
 export function configCommoditiesRoutes(router: Router) {
-  router.post('/commodities', async (req: Request, res: Response) => {
-    if (!req.body) {
-      return res.status(400).json({
-        message: 'Missing request body',
-      });
-    }
-
-    const { code, name, quantity } = req.body;
-    if (!code || !name || !quantity) {
-      return res.status(400).json({
-        message: 'Missing required fields',
-      });
-    }
-    const result = await commoditiesRepository.create({
-      code,
-      name,
-      quantity,
-    });
-    console.log(result);
-
-    return res.status(201).json({
-      data: result,
-    });
-  });
-
-  router.get('/commodities', async (req: Request, res: Response) => {
-    const result = await commoditiesRepository.findAll();
-    return res.status(200).json({
-      data: result,
-    });
-  });
-
-  router.get('/commodities/:id', async (req: Request, res: Response) => {
-     try {
-       const commodityId = req.params.id;
-       if (!commodityId) {
-         return res.status(400).json({
-           message: 'Missing commodity id',
-         });
-       }
-
-       const result = await commoditiesRepository.findById(String(commodityId));
-
-       if (!result) {
-         return res.status(404).json({
-           message: `Commodity with id ${commodityId} was not found`,
-         });
-       }
-
-       return res.status(200).json({
-         data: result,
-       });
-     } catch (error: any) {
-       console.log(error);
-       return res.status(200).json({
-         message: error.message,
-       });
-     }
-  });
-
-  router.put('/commodities/:id', async (req: Request, res: Response) => {
-    const commodityId = req.params.id;
-
-    if (!commodityId) {
-      return res.status(400).json({
-        message: 'Missing commodities id',
-      });
-    }
-
-    if (!req.body) {
-      return res.status(400).json({
-        message: 'Missing request body',
-      });
-    }
-
-    const { code, name, quantity } = req.body;
-    if (!code || !name || !quantity) {
-      return res.status(400).json({
-        message: 'Missing required fields',
-      });
-    }
-
-    const result = await commoditiesRepository.updateById(String(commodityId), {
-      code,
-      name,
-      quantity,
-    });
-
-    return res.status(200).json({
-      message: 'UPDATED',
-      data: result,
-    });
-  });
-
-  router.delete('/commodities/:id', async (req: Request, res: Response) => {
-    const commodityId = req.params.id;
-    if (!commodityId) {
-      res.status(400).json({
-        message: 'Missing commodity id',
-      });
-    }
-
-    const result = await commoditiesRepository.deleteById(String(commodityId));
-
-    return res.status(200).json({
-      message: 'DELETED',
-      data: result,
-    });
-  });
+  router.post('/commodities', expressHttpAdapter(commoditiesController.create));
+  router.get('/commodities', expressHttpAdapter(commoditiesController.findAll));
+  router.get('/commodities/:id', expressHttpAdapter(commoditiesController.findById));
+  router.put('/commodities/:id', expressHttpAdapter(commoditiesController.updateById));
+  router.delete('/commodities/:id', expressHttpAdapter(commoditiesController.deleteById));
 }
